f2a70a56e27b598fcb1c1d290a1ee66bd9c21c99
Trungtd0304 <trungtd0304@gmail.com>
2023-11-10 14:29:57 +0700

    update code area tay doi ten

--- area.py
+++ area.py
@@ -5,13 +5,16 @@ def app(df, first_date, latest_date, customer, id_kh, ne_kh,id_kv,id_tinh):
     df=df.query(
         "date >= @first_date & date <= @latest_date & KH == @customer & (ma_khach_hang == @id_kh | nguon_dat_hang == @ne_kh) & (khu_vuc == @id_kv | tinh_gui == @id_tinh)"
     )
-    df_selection = df.groupby(['khu_vuc', 'date'])['total_order'].sum().reset_index()
+    rename_cot = {  'khu_vuc': 'Khu vực', 
+                'tinh_gui': 'Tỉnh gửi'}
+    df = df.rename(columns=rename_cot)
+    df_selection = df.groupby(['Khu vực', 'date'])['total_order'].sum().reset_index()
     df_selection['date']=df_selection['date'].dt.strftime('%Y/%m/%d')
-    df_pivot_KV_number = pd.pivot_table(data=df_selection,index=['khu_vuc'], columns='date', values='total_order',aggfunc=sum,fill_value=0)
+    df_pivot_KV_number = pd.pivot_table(data=df_selection,index=['Khu vực'], columns='date', values='total_order',aggfunc=sum,fill_value=0)
     df_pivot_KV_number['Tổng'] = df_pivot_KV_number.iloc[:, 0:].sum(axis=1)
     # st.write(df_pivot_KV_number)    
-    # Thêm % total_order theo khu_vuc
-    df_pivot_KV_Ratio= pd.pivot_table(data=df_selection, index='khu_vuc',
+    # Thêm % total_order theo Khu vực
+    df_pivot_KV_Ratio= pd.pivot_table(data=df_selection, index='Khu vực',
                                 columns='date',
                                 values='total_order',
                                 aggfunc=sum,
@@ -28,11 +31,11 @@ def app(df, first_date, latest_date, customer, id_kh, ne_kh,id_kv,id_tinh):
     with tab2:
         st.write(df_pivot_KV_Ratio)
     
-    df_selection_tinh = df.groupby(['tinh_gui', 'date'])['total_order'].sum().reset_index()
+    df_selection_tinh = df.groupby(['Tỉnh gửi', 'date'])['total_order'].sum().reset_index()
     df_selection_tinh['date']=df_selection_tinh['date'].dt.strftime('%Y/%m/%d')
-    df_pivot_Tinh_number = pd.pivot_table(data=df_selection_tinh,index=['tinh_gui'], columns='date', values='total_order',aggfunc=sum,fill_value=0)
+    df_pivot_Tinh_number = pd.pivot_table(data=df_selection_tinh,index=['Tỉnh gửi'], columns='date', values='total_order',aggfunc=sum,fill_value=0)
     df_pivot_Tinh_number['Tổng'] = df_pivot_Tinh_number.iloc[:, 0:].sum(axis=1)
-    df_pivot_Tinh_Ratio= pd.pivot_table(data=df_selection_tinh, index='tinh_gui',
+    df_pivot_Tinh_Ratio= pd.pivot_table(data=df_selection_tinh, index='Tỉnh gửi',
                                 columns='date',
                                 values='total_order',
                                 aggfunc=sum,
